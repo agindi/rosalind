8c04db3704e3c5d8eb284baa9cecd67e9e7be5b6
agindi <aarongindi@gmail.com>
2022-02-06 14:54:14 -0500

    Start Complementing a Strand of DNA

---
+++ Complementing a Strand of DNA/code.py
@@ -0,0 +1,15 @@
+import os
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+def main():
+    with open(os.path.join(__location__, '.\data.dat')) as f:
+        line = f.readlines()[0]
+        nline = line.replace('T', 'U')
+        print(nline)
+
+
+
+
+if __name__ == "__main__":
+    main()
