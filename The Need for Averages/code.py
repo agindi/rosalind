1fd0b90306f6ce61a172fb971e84f4f093391e15
agindi <aarongindi@gmail.com>
2022-02-07 18:20:53 -0500

    Start The Need for Averages

---
+++ The Need for Averages/code.py
@@ -0,0 +1,13 @@
+import os
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+def main():
+    with open(os.path.join(__location__, '.\data.dat')) as f:
+        line = f.readlines()[0]
+        nums = line.split(' ')
+        
+
+
+if __name__ == "__main__":
+    main()
