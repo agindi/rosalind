243494baf00af4a6dad059aa8fdea06232fe9bc6
agindi <aarongindi@gmail.com>
2022-02-23 20:27:49 -0500

    Complete GC Content (complicated, feels pythonic)

--- Computing GC Content/code.py
+++ Computing GC Content/code.py
@@ -4,21 +4,32 @@ __location__ = os.path.realpath(
 
 def main():
     with open(os.path.join(__location__, '.\data.dat')) as f:
-        line = f.readlines()[0]
-        nline = list()
-        for c in line[::-1]:
-            if c == 'A':
-                nline.append('T')
-            elif c == 'T':
-                nline.append('A')
-            elif c == 'C':
-                nline.append('G')
-            elif c == 'G':
-                nline.append('C')
-        output = ''.join(nline)
-        print(output)
+        lines = f.readlines()
+        inputMap = parseFASTA(lines)
+        maxItem = max(inputMap.items(), key = lambda k : computeGCContent(k[1]))
+        print(maxItem[0])
+        print(round(100*computeGCContent(maxItem[1]), 4))
 
+def computeGCContent(seq):
+    compiler = {'A': 0, 'T': 0, 'G': 0, 'C': 0}
+    for base in seq:
+        compiler[base] += 1
+    return (compiler['C'] + compiler['G']) / sum(compiler.values())
 
+def parseFASTA(inputList):
+    outputMap = dict()
+    currID = inputList[0][:-1]
+    buffer = ""
+    for line in inputList[1:]:
+        line = line.strip()
+        if (line[0] == '>'):
+            outputMap[currID] = buffer + ""
+            buffer = ""
+            currID = line[1:]
+        else:
+            buffer = buffer + line
+    outputMap[currID] = buffer + ""
+    return outputMap
 
 
 if __name__ == "__main__":
